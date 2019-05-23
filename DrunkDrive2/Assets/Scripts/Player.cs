0c8e752ca337435448dee7b535a56c585a406a11
JoeSB <joe.sb.uc@gmail.com>
2019-05-22 20:14:36 -0500

    Primer commit del juego

---
+++ DrunkDrive2/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public enum Direction
+    {
+        Right = 1,
+        Left = -1
+    }
+
+    [SerializeField]
+    float RotationSpeed;
+    [SerializeField]
+    float MovementSpeed;
+
+    Direction direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Direction.Right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(direction == Direction.Right)
+            {
+                direction = Direction.Left;
+            }
+            else
+            {
+                direction = Direction.Right;
+            }
+        }
+        gameObject.transform.Translate(0, 0, -MovementSpeed * Time.deltaTime);
+        gameObject.transform.Rotate(0, (RotationSpeed * (int) direction) * Time.deltaTime, 0);
+    }
+}
