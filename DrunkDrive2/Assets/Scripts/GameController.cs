11605714dcd84172bdf419502af012d502bd5a79
JoeSB <joe.sb.uc@gmail.com>
2019-06-04 00:14:42 -0500

    Se termino la logica del gameController

---
+++ DrunkDrive2/Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    //Añadir funcionalidad para que al dar tap continue al siguiente juego en automatico
+    public static GameController instance;
+    private bool theGameIsOver = false;
+
+    [SerializeField]
+    private GameObject gameOverScreen;
+    [SerializeField]
+    private Text txt_gameOver;
+    [SerializeField]
+    private Color color;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+
+    }
+
+    public void WinGame()
+    {
+        theGameIsOver = true;
+        gameOverScreen.SetActive(true);
+        txt_gameOver.text = "You Win :D";
+    }
+
+    public void LoseGame()
+    {
+        theGameIsOver = true;
+        gameOverScreen.SetActive(true);
+        txt_gameOver.text = "You Lose :(";
+    }
+}
---
+++ DrunkDrive2/Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadScene(int _index)
+    {
+        SceneManager.LoadScene(_index, LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
